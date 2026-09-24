e6a38c7082c1919650ec19dde357a7705dd1dded
Ahmed Ali <ahmed.ali@autotrader.co.uk>
2026-09-24 10:18:51 +0100

    using operators

--- src/main/java/org/mdigital/Main.java
+++ src/main/java/org/mdigital/Main.java
@@ -6,6 +6,25 @@ package org.mdigital;
 
 public class Main {
     static void main() {
-        System.out.println("Hi");
+        System.out.println("It's Thursday Today");
+        /*
+        Operators in Java
+        * = multiply
+        - = subtraction
+        + = addition
+        / = division
+        < = less than
+        > = greater than
+         */
+
+        int number1 = 10;
+        int number2 = 10;
+        int result = number1 + number2;
+        System.out.println(result);
+
+        int number3 = 10;
+        int number4 = 10;
+        int result2 = number3 / number4;
+        System.out.println(result2);
     }
 }
