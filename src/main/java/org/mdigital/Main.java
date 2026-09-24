03775e441d83ecfcdf4edced4ab527224e719a91
Ahmed Ali <ahmed.ali@autotrader.co.uk>
2026-09-24 11:09:45 +0100

    learnt more operators

--- src/main/java/org/mdigital/Main.java
+++ src/main/java/org/mdigital/Main.java
@@ -1,20 +1,21 @@
 /*
 * Manchester Digital Session 3
+* Operators in Java
 */
 
 package org.mdigital;
 
 public class Main {
     static void main() {
-        System.out.println("It's Thursday Today");
+        System.out.println("It's Thursday Today!");
+
         /*
-        Operators in Java
-        * = multiply
-        - = subtraction
+         Math Operators
+        * = multiply (2*2 = 4)
+        - = subtraction (3-2 = 1)
         + = addition
         / = division
-        < = less than
-        > = greater than
+        % = mod or modulo
          */
 
         int number1 = 10;
@@ -22,9 +23,47 @@ public class Main {
         int result = number1 + number2;
         System.out.println(result);
 
-        int number3 = 10;
-        int number4 = 10;
-        int result2 = number3 / number4;
-        System.out.println(result2);
+        int number3 = 3;
+        int number4 = 20;
+        int result2 = number4 / number3;
+        int reminder = number4 % number3;
+        System.out.println(result2 + " reminder " + reminder);
+
+        /*
+        Increment & Decrement
+        ++ = increment by one
+        -- = decrement by one
+         */
+
+        int x = 1;
+        System.out.println(++x); // 2
+        System.out.println(x); // 2
+        System.out.println(x++); // 2 (ideal)
+        System.out.println(x); // 3
+        System.out.println(x--); // 2
+
+        /*
+        Comparison Operators
+        == = equal to
+        .equals() = strict equal to
+        != = not equal to
+        !object.equals() = strict not equal to
+        > = greater than
+        < = less than
+        >= = greater than or equal to
+        <= = less than or equal to
+         */
+
+        int num1 = 2;
+        String num2 = "2";
+        System.out.println("2".equals(num2));
+
+        /*
+        Logical Operators
+        && = AND
+        || = OR
+        ! = NOT
+         */
+
     }
 }
